953a2b5e3f16b520f44ed47b3f087cff386a5ad3
Dima_Golt <dimagolt@gmail.com>
2023-06-23 01:23:15 +0200

    add UI for new stats

--- main.cpp
+++ main.cpp
@@ -6,29 +6,54 @@
 bool displayOn = true;
 std::vector<std::shared_ptr<Monke>> chefs;
 
-void display(Kitchen& kitchen) {
+void display(Kitchen &kitchen)
+{
     noecho();
+
+    bool isMonkeyDesc = false;
+    int monkeClicked = 0;
+    int ch = 0;
 
     while (displayOn)
     {
         erase();
-        mvprintw(0,0,"Malpo mlyniarzu, co robisz. Press ESC to exit");
-        mvprintw(1, 0,"_____________________________________________________________________________________________");
-        mvprintw(2, 0,"|____________Monkey_does____________________|_Monkey_hungry_|_______Recipe_______|_Time_left_|");
+
+        ch = getch();
+
+        MEVENT mEvent;
+        if (getmouse(&mEvent) == OK)
+        {
+            if (mEvent.y < chefs.size() + 3 && mEvent.y > 2)
+            {
+                isMonkeyDesc = true;
+                monkeClicked = mEvent.y - 2;
+            }
+            else
+            {
+                isMonkeyDesc = false;
+                monkeClicked = 0;
+            }
+        }
+
+        mvprintw(0, 0, "Malpo mlyniarzu, co robisz. Press ESC to exit");
+        mvprintw(1, 0, "_____________________________________________________________________________________________");
+        mvprintw(2, 0, "|____________Monkey_does____________________|_Monkey_hungry_|_______Recipe_______|_Time_left_|");
         int y = 3;
-        for (int i = 0; i < chefs.size(); i++) {
+        for (int i = 0; i < chefs.size(); i++)
+        {
             Monke *monke = chefs[i].get();
             mvprintw(y, 1, "Monke %d", monke->getId());
             mvprintw(y, 0, "|");
             mvprintw(y, 46, "__________");
-            if(y<12)
+            if (y < 12)
                 mvprintw(y, 9, monke->getStatus().c_str());
             else
                 mvprintw(y, 10, monke->getStatus().c_str());
-            for(int j=0; j<monke->getHungerLevel(); j++){
-                mvprintw(y, 46+(j/10), "#");
+            for (int j = 0; j < monke->getHungerLevel(); j++)
+            {
+                mvprintw(y, 46 + (j / 10), "#");
             }
-            mvprintw(y, 57, "%d%%",monke->getHungerLevel());
+            mvprintw(y, 57, "%d%%", monke->getHungerLevel());
             mvprintw(y, 62, monke->getRecipe().getName().c_str());
             mvprintw(y, 87, "%d", monke->getTimeLeft());
             mvprintw(y, 44, "|");
@@ -38,35 +63,59 @@ void display(Kitchen& kitchen) {
             y++;
         }
 
-        mvprintw(y, 0,"|____________________________________________________________________________________________|");
+        mvprintw(y, 0, "|____________________________________________________________________________________________|");
         auto itemAmount = kitchen.getItemAmount();
-        for (auto& pair : kitchen.getHowManyUsedRightNow())
-        {
-            mvprintw(++y, 1,"%ss: %d/%d", pair.first.c_str(), pair.second, itemAmount[pair.first]);
-        }
 
-        mvprintw(++y, 1, "Helping monkes: %d", kitchen.helpingMonkes);
-
-        for (auto& monkePair : kitchen.getHelpingMonkes(chefs))
+        mvprintw(++y, 0, "|___________________________________________|________________________________________________|");
+        int yForReturn = y;
+        int yUsage = y;
+        if (isMonkeyDesc)
         {
-            mvprintw(++y, 1,"Monke %d is helping monke %d", monkePair.first, monkePair.second);
-        }
-
-        for(auto& monke : chefs)
-        {
-            for(auto& pair : monke->getTimesUsingItem())
+            mvprintw(y, 14, "Monkey_%d_usage", monkeClicked);
+            for (auto &pair : chefs[monkeClicked - 1]->getTimesUsingItem())
             {
-                mvprintw(++y, 1, "Monke %d used %s : %d times", monke->getId(), pair.first.c_str(), pair.second);
+                mvprintw(++y, 1, "Monke %d used %s : %d times", monkeClicked, pair.first.c_str(), pair.second);
+                mvprintw(y, 0, "|");
+                mvprintw(y, 44, "|");
+                mvprintw(y, 93, "|");
             }
         }
+        else
+        {
+            mvprintw(y, 14, "All_kitchen_items");
+            for (auto &pair : kitchen.getHowManyUsedRightNow())
+            {
+                mvprintw(++y, 1, "%ss: %d/%d", pair.first.c_str(), pair.second, itemAmount[pair.first]);
+                mvprintw(y, 0, "|");
+                mvprintw(y, 44, "|");
+                mvprintw(y, 93, "|");
+            }
+        }
+        yUsage = y;
 
+        y = yForReturn;
+
+        mvprintw(y, 65, "Helping_monkes:_%d", kitchen.helpingMonkes);
+
+        for (auto &monkePair : kitchen.getHelpingMonkes(chefs))
+        {
+            mvprintw(++y, 50, "Monke %d is helping monke %d", monkePair.first, monkePair.second);
+            mvprintw(y, 0, "|");
+            mvprintw(y, 44, "|");
+            mvprintw(y, 93, "|");
+        }
+
+        if (yUsage > y)
+            y = yUsage;
+
+        mvprintw(++y, 0, "|____________________________________________________________________________________________|");
         refresh();
         usleep(10000);
     }
-
 }
 
-int main() {
+int main()
+{
 
     uint32_t MONKE_NUM = 6;
     uint32_t HUNGER_DEPLETION_TIME = 6;
@@ -79,7 +128,11 @@ int main() {
     std::cout << "\nProvide hunger depletion value (from 1 to 99): ";
     std::cin >> HUNGER_DEPLETION_AMOUNT;
 
-    initscr();
+    WINDOW *w = initscr();
+    mousemask(ALL_MOUSE_EVENTS, NULL);
+    mouseinterval(0);
+    keypad(w, TRUE);
+    nodelay(w, TRUE);
     cbreak();
     refresh();
 
@@ -98,21 +151,21 @@ int main() {
         {
             auto chefPtr = std::make_shared<Monke>(i + 1, kitchen, &chefs, HUNGER_DEPLETION_TIME, HUNGER_DEPLETION_AMOUNT);
             chefs.emplace_back(chefPtr);
-            chefThreads.emplace_back([chefPtr]() { (*chefPtr)(); });
+            chefThreads.emplace_back([chefPtr]()
+                                     { (*chefPtr)(); });
         }
 
-        for (auto &thread: chefThreads)
+        for (auto &thread : chefThreads)
         {
             thread.join();
         }
-
     }
-    catch(MonkeUtility::MonkeException& e)
+    catch (MonkeUtility::MonkeException &e)
     {
         std::cout << e.what();
         return e.getErrorCode();
     }
-    catch(std::exception& e)
+    catch (std::exception &e)
     {
         std::cout << e.what();
         return 199;
